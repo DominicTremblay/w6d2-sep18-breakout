960b2b8ec45d27ea2e3e8794604d963748ad0699
Dominic Tremblay <dominictremblay24@gmail.com>
2018-09-04 01:18:30 -0400

    made the fetch request and added the tweets to the state

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,19 @@ import NewTweet from "./NewTweet";
 import AllTweets from "./AllTweets";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tweets: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("/tweets")
+      .then(response => response.json())
+      .then(tweets => this.setState({ tweets: tweets }));
+  }
+
   render() {
     return (
       <div>
